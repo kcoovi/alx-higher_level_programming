51bb62a7b65c2247de87fe12e7b826087bc8c713
Kevin Coovi <k.coovi@gmail.com>
2023-06-05 16:13:32 +0300

    Print object-oriented programming with Python, followed by a new line

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming " \
-      "language that combines remarkable power with very clear syntax"
-str_list = [str[i] for i in range(39, 67)] + [str[i] for i in range(107, 112)] + [str[i] for i in range(6)]
-str = ''.join(str_list)
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
