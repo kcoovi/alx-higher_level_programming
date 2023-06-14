d870e5c9b21b35622645c172fed48166bdc14c6d
Kevin COOVI <k.coovi@gmail.com>
2023-06-14 14:22:54 +0300

    A function that returns a list with all values multiplied by a number without using any loops

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,40 @@
-#!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+def subtract_max_value(num_list):
+    subtract_val = 0
+    max_val = max(num_list)
+
+    for num in num_list:
+        if max_val > num:
+            subtract_val += num
+
+    return max_val - subtract_val
+
+
+def roman_to_int(roman_str):
+    if not roman_str:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                  'C': 100, 'D': 500, 'M': 1000}
+
+    if not isinstance(roman_str, str):
+        return 0
+
+    roman_nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                  'D': 500, 'M': 1000}
+    roman_keys = list(roman_nums.keys())
+
     result = 0
-    for i in range(len(roman_string)):
-        if i > 0
-        and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            result += (roman_dict[roman_string[i]] -
-                       2 * roman_dict[roman_string[i - 1]])
-        else:
-            result += roman_dict[roman_string[i]]
+    last_val = 0
+    num_list = [0]
+
+    for char in roman_str:
+        for r_key in roman_keys:
+            if r_key == char:
+                if roman_nums.get(char) <= last_val:
+                    result += subtract_max_value(num_list)
+                    num_list = [roman_nums.get(char)]
+                else:
+                    num_list.append(roman_nums.get(char))
+
+                last_val = roman_nums.get(char)
+
+    result += subtract_max_value(num_list)
+
     return result
