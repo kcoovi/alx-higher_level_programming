66ddcdaaa33ee708319a3040fd40f6cfa8d631ea
Kevin COOVI <k.coovi@gmail.com>
2023-07-10 12:19:32 +0300

    A class BaseGeometry (based on 6-base_geometry.py)

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Function"""
+"""
+Contains the class BaseGeometry
+"""
 
 
 class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
     def area(self):
-        """Doc"""
+        """raises an exception when called"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Doc"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
