28502db7a97b1fb0f663e80b209adc3c33c323f5
Kevin COOVI <k.coovi@gmail.com>
2023-07-10 12:29:44 +0300

    A function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-Contains the class BaseGeometry
-"""
+"""Main"""
 
 
 class BaseGeometry:
-    """A class with public instance methods area and integer_validator"""
+    """Doc"""
     def area(self):
-        """raises an exception when called"""
+        """Doc"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates that value is an integer greater than 0"""
+        """Doc"""
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
