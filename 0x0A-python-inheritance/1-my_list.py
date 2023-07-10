06466c9c225fc0133ea8cdcda08df2035de5675e
Kevin COOVI <k.coovi@gmail.com>
2023-07-10 12:12:18 +0300

    A class MyList that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""Function"""
+"""Main"""
 
 
 class MyList(list):
+    """Doc"""
+    def __init__(self):
+        """Doc"""
+        super().__init__()
+
     def print_sorted(self):
         """Doc"""
         print(sorted(self))
