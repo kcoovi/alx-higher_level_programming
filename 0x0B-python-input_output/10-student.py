883fb2feac336dba7ecba839e9826ac0f47a5a27
Kevin COOVI <k.coovi@gmail.com>
2023-07-11 12:24:07 +0300

    A class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Main"""
+
+
+class Student():
+    """Doc"""
+
+    def __init__(self, first_name, last_name, age):
+        """Doc"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Doc"""
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return (self.__dict__)
